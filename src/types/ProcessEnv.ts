702bb462893b8559bdb2a92f6605de3db08637a8
iyobo <phil.eki@gmail.com>
2023-05-25 16:42:41 -0500

    App configs

--- src/types/ProcessEnv.ts
+++ src/types/ProcessEnv.ts
@@ -6,15 +6,6 @@ declare global {
       BASE_URL: string
       PORT?: string
 
-      DATABASE_URL?: string
-      DB_HOST?: string
-      DB_PORT?: string
-      DB_NAME?: string
-      DB_TYPE?: string
-      DB_USER?: string
-      DB_PASSWORD?: string
-      DB_SSL?: string
-
       S3_ACCESS_KEY: string
       S3_ACCESS_SECRET: string
       S3_BUCKET: string
@@ -22,19 +13,6 @@ declare global {
 
       SECURITY_KEY_1: string
       SECURITY_KEY_2: string
-
-      AUTH_GOOGLE_KEY: string
-      AUTH_GOOGLE_SECRET: string
-      AUTH_FACEBOOK_KEY: string
-      AUTH_FACEBOOK_SECRET: string
-
-      MAIL_SPARKPOST_API_KEY: string
-      MAIL_DEFAULT_ADDRESS: string
-      MAIL_DEFAULT_NAME: string
-
-      SMS_DEFAULT_PHONE: string
-      SMS_TWILIO_KEY: string
-      SMS_TWILIO_SECRET: string
 
       ONBOARDING_URL: string
     }
