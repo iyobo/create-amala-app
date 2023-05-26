8e845a8d841541f4d4dd22cdb91a9266ea05544b
iyobo <phil.eki@gmail.com>
2023-05-26 08:12:25 -0500

    storage tinz

--- bin/main.js
+++ bin/main.js
@@ -1,16 +1,22 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const src = path.resolve(__dirname,'..')
-const dest = process.argv.slice(2)[0];
-console.log('From:', src);
-console.log('To:', dest);
+
 
 async function main() {
+  const {default: chalk} = await import('chalk')
+  chalk.white('Amala')
+
+  console.log('\nBeginning Amala app scaffold~~~\n')
+  const src = path.resolve(__dirname,'..')
+  const dest = process.argv.slice(2)[0];
+  console.log('From:', src);
+  console.log('To:', dest);
+
   console.log(`Ensuring directory ${dest} exists...`)
   await fs.mkdir(dest, { recursive: true });
 
-  console.log('Copying...')
+  console.log('Copying files...')
   const files = await fs.readdir(src)
 
 
@@ -24,15 +30,15 @@ async function main() {
     if(file === '.git') continue
     if(file === '.idea') continue
 
-    console.log('copy:', file, path.resolve(dest,file))
+    console.log('  copying:', file, ' --> ',path.resolve(dest,file))
     await fs.cp(file, path.resolve(dest,file), { recursive: true });
   }
 
-  console.log('Installing packages...')
+  console.log('\nInstalling dependencies...')
   const {execSync} = require('child_process');
   execSync(`cd ${dest} && npm install`)
 
-  console.log('Finished!')
+  console.log('\nFinished!\n')
 
 }
 
